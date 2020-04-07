7168d1ef25db69392c8062d7efc536b2fb499716
nobodyzxc <nobodyzxc.tw@gmail.com>
2020-04-08 00:36:51 +0800

    update

--- craft.py
+++ craft.py
@@ -1,28 +1,30 @@
-#http://patft.uspto.gov/netacgi/nph-Parser?Sect2=PTO1&Sect2=HITOFF&p=1&u=/netahtml/PTO/search-bool.html&r=1&f=G&l=50&d=PALL&RefSrch=yes&Query=PN/10608886
 import Patent_Crawler as ptc
 import sys, csv
+from tqdm import tqdm
 
 nos = open(sys.argv[1], 'r').read().split()
 
-with open('output.csv', 'w', newline='') as csvfile:
-  writer = csv.writer(csvfile)
-  writer.writerow(['No.', 'Abstract', 'Claims'])
+with open('error.txt', 'w', newline='') as errfile:
+    with open('output.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['No.', 'Abstract', 'Claims'])
+    
+        for no in tqdm(nos):
+            try:
+                _, PatFT_link, _, _, _ = ptc.PN_str_and_url(no)
+                soup = ptc.url2soup(PatFT_link)
+                
+                abstract = soup.find('b', text='Abstract')\
+                                .parent.find_next_siblings('p')[0].string
+                
+                claims = ''
+                claim_switch = False
 
+                for tag in soup.find('center', text='Claims').next_siblings:
+                    if tag.name == 'center': break
+                    if tag.string: claims += tag.string
 
-  for no in nos:
-      _, PatFT_link, _, _, _ = ptc.PN_str_and_url(no)
-      soup = ptc.url2soup(PatFT_link)
-      
-      abstract = soup.find('b', text='Abstract')\
-              .parent.find_next_siblings('p')[0].string
-      
-      claims = ''
-      claim_switch = False
-
-      for tag in soup.find('coma'):
-          if tag.name == 'center':
-              claim_switch = tag.string == 'Claims'
-          if claim_switch and tag.string:
-              claims += tag.string
-
-      writer.writerow([no, abstract, claims])
+                writer.writerow([no, abstract, claims])
+            except Exception as e:
+                print("{} error: {}".format(no, e))
+                errfile.write("{} error: {}\n".format(no, e))
